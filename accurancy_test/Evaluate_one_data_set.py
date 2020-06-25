772a6951e4bf1dcd53f238ac4397c3499a6a55c9
jbrugger <wwjbrugger@gmail.com>
2020-06-25 22:38:58 +0100

    results of dcdl test in one frame

--- accurancy_test/Evaluate_one_data_set.py
+++ accurancy_test/Evaluate_one_data_set.py
@@ -7,6 +7,7 @@ import statistics
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 def return_label(path):
     return int(path[6])
@@ -44,8 +45,9 @@ def average_similarity_single_label(tabels, label, dataset):
         x_values.append(value[-1])
     graph_with_error_bar(x_values, y_values, y_stdr, titel, fix_y_axis=True, x_axis_title="label", y_axis_tile='similarity [%]')
 
-def average_accurancy_on_test_data(tabels, dataset):
-    titel = 'average accurancy on test data \n {}'.format(dataset)
+def average_accurancy_on_test_data(tabels, titel, ax):
+    if titel:
+        titel = 'average accurancy on test data \n {}'.format(titel)
     deep_rule_set = []
     sls_black_box_prediction = []
     sls_black_box_label = []
@@ -62,11 +64,11 @@ def average_accurancy_on_test_data(tabels, dataset):
 
 
 
-    x_values = ['Deep_Rule_Set', 'SLS_Black_\nBox_Prediction', 'SLS_Black_\nBox_Label', 'Neural Network']
+    x_values = ['DRS', 'BB\nPrediction', 'BB\nLabel', 'NN']
     y_values = [mean_deep_rule_set,  mean_sls_black_box_prediction, mean_sls_black_box_label, mean_neural_net]
     y_stdr=[stdr_deep_rule_set, stdr_sls_black_box_prediction, stdr_sls_black_box_label, stdr_neural_net]
-    graph_with_error_bar(x_values, y_values, y_stdr, titel, fix_y_axis=True, x_axis_title="method",
-                         y_axis_tile='similarity [%]')
+    graph_with_error_bar(x_values, y_values, y_stdr, titel, fix_y_axis=True, x_axis_title="",
+                         y_axis_tile='accuracy [%]', ax_out=ax)
 
 def concat_minus_SLS_prediction(tabels):
     Concat_minus_SLS_prediction = []
@@ -83,8 +85,11 @@ def calculate_mean_std(array):
     return mean, standard_derivation
 
 
-def graph_with_error_bar(x_values, y_values, y_stdr, title,x_axis_title="", y_axis_tile='', fix_y_axis= False ):
-    fig, ax = plt.subplots()
+def graph_with_error_bar(x_values, y_values, y_stdr, title,x_axis_title="", y_axis_tile='', fix_y_axis= False, ax_out = False ):
+    if not ax_out:
+        fig, ax = plt.subplots()
+    else:
+        ax = ax_out
     ax.errorbar(x_values, y_values,
                 yerr=y_stdr,
                 fmt='o')
@@ -93,22 +98,55 @@ def graph_with_error_bar(x_values, y_values, y_stdr, title,x_axis_title="", y_ax
     ax.set_title(title)
     if fix_y_axis:
         ax.set_ylim(0.5, 1)
+    if not ax_out:
+        plt.show()
+
+
+def three_graph_in_one():
+
+    gs = gridspec.GridSpec(4, 4)
+
+    ax1 = plt.subplot(gs[:2, :2])
+    ax1.plot(range(0, 10), range(0, 10))
+
+    ax2 = plt.subplot(gs[:2, 2:])
+    ax2.plot(range(0, 10), range(0, 10))
+
+    ax3 = plt.subplot(gs[2:4, 1:3])
+    ax3.plot(range(0, 10), range(0, 10))
+
     plt.show()
 
 
 if __name__ == '__main__':
+
     x_values = []
     y_values = []
     y_stdr = []
-    for path_to_results in ['data/numbers/results', 'data/fashion/results', 'data/cifar/results']:
-        print('\n\n' + "\033[1m" + path_to_results[5:-8] + "\033[0;0m" + '\n\n')
 
+    for i, path_to_results in enumerate(['data/numbers/results', 'data/fashion/results', 'data/cifar/results']):
+        print('\n\n' + "\033[1m" + path_to_results[5:-8] + "\033[0;0m" + '\n\n')
         table, label = load_tables(path_to_results)
+
         average_similarity_single_label(table, label, path_to_results[5:-8])
-        average_accurancy_on_test_data(table,path_to_results[5:-8])
+        #average_accurancy_on_test_data(table,path_to_results[5:-8], position[i])
         Conc_minus_SLS_pred = concat_minus_SLS_prediction(table)
         mean, stdr = calculate_mean_std(Conc_minus_SLS_pred)
         x_values.append(path_to_results[5:-8])
         y_values.append(mean)
         y_stdr.append(stdr)
     graph_with_error_bar(x_values, y_values, y_stdr, 'Deep_Rule_Set - SLS_Black_Box_Prediction \n label predicted from NN for train data', x_axis_title="Data set", y_axis_tile='similarity[%]' )
+
+    gs = gridspec.GridSpec(4, 4)
+    position = [plt.subplot(gs[:2, :2]), plt.subplot(gs[:2, 2:]), plt.subplot(gs[2:4, 1:3])]
+    for i, path_to_results in enumerate(['data/numbers/results', 'data/fashion/results', 'data/cifar/results']):
+        print('\n\n' + "\033[1m" + path_to_results[5:-8] + "\033[0;0m" + '\n\n')
+        table, label = load_tables(path_to_results)
+        average_accurancy_on_test_data(table, '', position[i])
+        #average_accurancy_on_test_data(table,path_to_results[5:-8], position[i])
+
+
+    plt.tight_layout()
+    #plt.figure(dpi=300)
+    plt.savefig('/home/jbrugger/IdeaProjects/DCDL_smaller/accurancy_test/results/accuracy_different_approaches.png', dpi = 300)
+    plt.show()
