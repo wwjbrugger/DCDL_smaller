6114db422d4c430bce050981b6b57d28d1d25117
jbrugger <wwjbrugger@gmail.com>
2020-08-03 12:30:53 +0100

    add students t test to evaluate_result_frame

--- comparison_DCDL_vs_SLS/evaluate_result_frame.py
+++ comparison_DCDL_vs_SLS/evaluate_result_frame.py
@@ -6,6 +6,9 @@ import pandas as pd
 import statistics
 import sys
 import numpy as np
+from scipy.stats import stats
+
+import helper_methods as help
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
@@ -25,34 +28,40 @@ def load_tables(path_to_results):
         path = join(path_to_results, file_name)
         pd_file = pickle.load(open(path, "rb"))
         pd_file = pd_file.rename_axis(file_name[:7], axis=1)
-        print(pd_file)
+        pd_file['label'] = file_name[:7] +'_'+ path_to_results.split('/')[1]
+        #print(pd_file)
         tables.append(pd_file)
     return tables, label
 
 
-def average_similarity_single_label(tabels, label, dataset):
-    titel = 'Deep_Rule_Set similarity \n   label predicted from NN for train data \n'+ dataset
-    d = defaultdict(list)
-    x_values=[]
-    y_values=[]
-    y_stdr=[]
-    for i in range (len(tabels)):
-        d[label[i]].append(tabels[i].at[0, 'Concat'])
-    for value in d:
-        m, s = calculate_mean_std(d[value])
-        y_values.append(m)
-        y_stdr.append(s)
-        x_values.append(value[-1])
-    graph_with_error_bar(x_values, y_values, y_stdr, titel, fix_y_axis=True, x_axis_title="label", y_axis_tile='similarity [%]')
+def sim_DCDL_NN():
+    for i, dataset in enumerate(['mnist', 'fashion', 'cifar']):
+        path_to_results = join('data', dataset, 'results')
+        titel = 'Deep_Rule_Set similarity \n   label predicted from NN for train data \n'+ dataset
+        table, label = load_tables(path_to_results)
+        d = defaultdict(list)
+        x_values=[]
+        y_values=[]
+        y_stdr=[]
+        for i in range (len(table)):
+            d[label[i]].append(table[i].at[0, 'Concat'])
+        for value in d:
+            m, s = calculate_mean_std(d[value])
+            y_values.append(m)
+            y_stdr.append(s)
+            x_values.append(value[-1])
+        help.graph_with_error_bar(x_values, y_values, y_stdr, titel, fix_y_axis=True, x_axis_title="label", y_axis_tile='similarity [%]',
+                                  save_path='results/comparision_DCDL_vs_SLS/sim_DCDL_NN_{}.png'.format(dataset))
 
-def average_accurancy_on_test_data(tabels, titel, ax):
+def average_accurancy_on_test_data(path_to_results, titel, ax):
+    tables, label = load_tables(path_to_results)
     if titel:
         titel = 'average accurancy on test data \n {}'.format(titel)
     deep_rule_set = []
     sls_black_box_prediction = []
     sls_black_box_label = []
     neural_net = []
-    for table in tabels:
+    for table in tables:
         deep_rule_set.append(table.at[3,'Concat'])
         sls_black_box_prediction.append(table.at[3,'SLS prediction'])
         sls_black_box_label.append(table.at[3, 'SLS train'])
@@ -62,95 +71,87 @@ def average_accurancy_on_test_data(tabels, titel, ax):
     mean_neural_net, stdr_neural_net = calculate_mean_std(neural_net)
     mean_sls_black_box_label, stdr_sls_black_box_label = calculate_mean_std(sls_black_box_label)
 
-
-
     x_values = ['DCDL', 'BB\nPrediction', 'BB\nLabel', 'NN']
     y_values = [mean_deep_rule_set,  mean_sls_black_box_prediction, mean_sls_black_box_label, mean_neural_net]
     y_stdr=[stdr_deep_rule_set, stdr_sls_black_box_prediction, stdr_sls_black_box_label, stdr_neural_net]
-    graph_with_error_bar(x_values, y_values, y_stdr, titel, fix_y_axis=True, x_axis_title="",
+    help.graph_with_error_bar(x_values, y_values, y_stdr, titel, fix_y_axis=True, x_axis_title="",
                          y_axis_tile='accuracy [%]', ax_out=ax)
 
-def concat_minus_SLS_prediction(tabels):
-    Concat_minus_SLS_prediction = []
-    for pd_file in tabels:
-        Concat_minus_SLS_prediction.append(pd_file.at[0, 'Concat'] - pd_file.at[0, 'SLS prediction'])
-    return Concat_minus_SLS_prediction
+def DCDL_minus_SLS_prediction():
+    x_values = []
+    y_values = []
+    y_stdr = []
+
+    for i, dataset in enumerate(['mnist', 'fashion', 'cifar']):
+        path_to_result = join('data', dataset, 'results')
+        table, label = load_tables(path_to_result)
+        Concat_minus_SLS_prediction = []
+        for pd_file in table:
+            Concat_minus_SLS_prediction.append(pd_file.at[0, 'Concat'] - pd_file.at[0, 'SLS prediction'])
+        mean, stdr = calculate_mean_std(Concat_minus_SLS_prediction)
+
+        x_values.append(dataset)
+        y_values.append(mean)
+        y_stdr.append(stdr)
+    help.graph_with_error_bar(x_values, y_values, y_stdr, title='', x_axis_title=" ", y_axis_tile='sim. diff. DCDL - SLS [%]',
+                         save_path= 'results/comparision_DCDL_vs_SLS/similarity_diff_SLS_DCDL.png' )
 
 
 def calculate_mean_std(array):
     mean = statistics.mean(array)
     standard_derivation = statistics.stdev(array)
-    print('Mean: {}  '.format(mean))
-    print('standard derivation:', standard_derivation)
     return mean, standard_derivation
 
+def students_t_test():
+    for i, dataset in enumerate(['mnist', 'fashion', 'cifar']):
+        print ('\033[94m', '\n', dataset, ' students-t-test', '\033[0m')
+        path_to_result = join('data', dataset, 'results')
+        tables, label = load_tables(path_to_result)
+        methods = ['Concat', 'SLS prediction', 'SLS train', 'Neural network']
+        df2 = pd.DataFrame(0,index= methods, columns= methods, dtype=float)
+        for i in range(len(methods)):
+            df2.at[methods[i], methods[i]] = 1
+            for j in range(i+1, len(methods), 1):
+                col_1 = []
+                col_2 = []
+                for table in tables:
+                    col_1.append(table.at[3,methods[i]])
+                    col_2.append(table.at[3,methods[j]])
 
-def graph_with_error_bar(x_values, y_values, y_stdr, title,x_axis_title="", y_axis_tile='', fix_y_axis= False, ax_out = False , path_to_save = False):
-    if not ax_out:
-        fig, ax = plt.subplots()
-    else:
-        ax = ax_out
-    ax.errorbar(x_values, y_values,
-                yerr=y_stdr,
-                fmt='o')
-    ax.set_xlabel(x_axis_title)
-    ax.set_ylabel(y_axis_tile)
-    ax.set_title(title)
-    if fix_y_axis:
-        ax.set_ylim(0.5, 1)
-    if not ax_out and not path_to_save:
-        plt.show()
-    if path_to_save:
-        plt.savefig(path_to_save,  dpi=300)
+                t_statistic, two_tailed_p_test = stats.ttest_ind(col_1, col_2)
+                df2.at[methods[i], methods[j]] = two_tailed_p_test
+                df2.at[methods[j], methods[i]] = two_tailed_p_test
+                if two_tailed_p_test > 0.05:
+                    print('{} and {} can have th same mean p_value = {}'.format(methods[i], methods[j],
+                                                                                two_tailed_p_test))
+                else:
+                    print('Reject that {} and {}  have the same mean p_value = {}'.format(methods[i], methods[j],
+                                                                                          two_tailed_p_test))
+        with pd.option_context('display.precision', 2):
+            html = df2.style.applymap(help.mark_small_values).render()
+        with open('results/comparision_DCDL_vs_SLS/students-test_{}.html'.format(dataset), "w") as f:
+            f.write(html)
 
 
-def three_graph_in_one():
-
+def average_accurancy():
     gs = gridspec.GridSpec(4, 4)
+    position = [plt.subplot(gs[:2, :2]), plt.subplot(gs[:2, 2:]), plt.subplot(gs[2:4, 1:3])]
+    for i, dataset in enumerate(['mnist', 'fashion', 'cifar']):
+        path_to_results = join('data', dataset, 'results')
+        average_accurancy_on_test_data(path_to_results, '', position[i])
 
-    ax1 = plt.subplot(gs[:2, :2])
-    ax1.plot(range(0, 10), range(0, 10))
-
-    ax2 = plt.subplot(gs[:2, 2:])
-    ax2.plot(range(0, 10), range(0, 10))
-
-    ax3 = plt.subplot(gs[2:4, 1:3])
-    ax3.plot(range(0, 10), range(0, 10))
-
+    plt.tight_layout()
+    plt.savefig('results/comparision_DCDL_vs_SLS/accuracy_different_approaches.png', dpi=300)
     plt.show()
 
 
 if __name__ == '__main__':
 
-    x_values = []
-    y_values = []
-    y_stdr = []
+    students_t_test()
+    sim_DCDL_NN()
 
-    for i, path_to_results in enumerate(['data/mnist/results', 'data/fashion/results', 'data/cifar/results']):
-        print('\n\n' + "\033[1m" + path_to_results[5:-8] + "\033[0;0m" + '\n\n')
-        table, label = load_tables(path_to_results)
+    DCDL_minus_SLS_prediction()
 
-        average_similarity_single_label(table, label, path_to_results[5:-8])
-        #average_accurancy_on_test_data(table,path_to_results[5:-8], position[i])
-        Conc_minus_SLS_pred = concat_minus_SLS_prediction(table)
-        mean, stdr = calculate_mean_std(Conc_minus_SLS_pred)
-        x_values.append(path_to_results[5:-8])
-        y_values.append(mean)
-        y_stdr.append(stdr)
-    graph_with_error_bar(x_values, y_values, y_stdr, title='', x_axis_title=" ", y_axis_tile='sim. diff. DCDL - SLS [%]',
-                         path_to_save=
-            'results/similarity_diff_SLS_DRS.png' )
-
-    gs = gridspec.GridSpec(4, 4)
-    position = [plt.subplot(gs[:2, :2]), plt.subplot(gs[:2, 2:]), plt.subplot(gs[2:4, 1:3])]
-    for i, path_to_results in enumerate(['data/mnist/results', 'data/fashion/results', 'data/cifar/results']):
-        print('\n\n' + "\033[1m" + path_to_results[5:-8] + "\033[0;0m" + '\n\n')
-        table, label = load_tables(path_to_results)
-        average_accurancy_on_test_data(table, '', position[i])
-        #average_accurancy_on_test_data(table,path_to_results[5:-8], position[i])
+    average_accurancy()
 
 
-    plt.tight_layout()
-    #plt.figure(dpi=300)
-    plt.savefig('/home/jbrugger/IdeaProjects/DCDL_smaller/comparison_DCDL_vs_SLS/results/accuracy_different_approaches.png', dpi = 300)
-    plt.show()
